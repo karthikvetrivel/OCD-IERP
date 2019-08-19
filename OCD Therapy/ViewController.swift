c5c18e529c43153d62368f0d7c8dd8923c5d2c72
KarthikV <kvvetrivel@gmail.com>
2019-08-18 18:25:11 -0700

    primary commit

---
+++ OCD Therapy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCD Therapy
+//
+//  Created by KarsickKeep on 8/18/19.
+//  Copyright © 2019 KarsickKeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
